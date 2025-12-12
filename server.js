ff216cd2907d5f6f9cd0fa27fd6a8c5261ea050b
zerodavis05 <zero.davis05@gmail.com>
2025-12-13 05:11:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const ENABLE_THINKING_MODE = true; // Set to true to enable chat_template_kwargs
 
 // Model mapping (adjust based on available NIM models)
 const MODEL_MAPPING = {
-  'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
-  'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
+  'gpt-3.5-turbo': 'deepseek-ai/deepseek-r1-0528',
+  'gpt-4': 'deepseek-ai/deepseek-v3.1-terminus',
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
   'claude-3-opus': 'openai/gpt-oss-120b',
